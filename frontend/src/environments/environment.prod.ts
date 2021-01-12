be150a9c4cf73a462a87f920e4697bab0a10cb58
Desenvolvimento 04 <desenvolvimento04@servidordell.inspell.com.br>
2021-01-11 23:01:24 -0300

    environment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: true,
+    api_url: '',
+    api_report: '',
+    api_img:'',
+    firebase: {
+    },
+    debug: false,
+    log: {
+      auth: false,
+      store: false,
+    },
+  
+    smartadmin: {
+      api: null,
+      db: 'smartadmin-angular'
+    }
+  
+  };
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  api_url: 'http://127.0.0.1:8000/api',
+  api_report: '',
+  api_img:'',
+  firebase: {},
+  debug: false,
+  log: {
+    auth: false,
+    store: false,
+  },
+  smartadmin: {
+    api: null,
+    db: 'sis'
+  }
+};
